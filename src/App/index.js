c8421a3269729d54723e5e3d8d2eb191b0789211
Clay Coleman <jclaytoncoleman@gmail.com>
2019-06-15 16:20:32 +0100

    routes and stuff set up

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../pages/LandingPage';
+
+import * as ROUTES from '../constants/routes';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import PasswordForgetPage from '../pages/PasswordForgetPage';
+import HomePage from '../pages/HomePage';
+import AccountPage from '../pages/AccountPage';
+import AdminPage from '../pages/AdminPage';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.LOGIN} component={LoginPage} />
+      <Route exact path={ROUTES.REGISTER} component={RegisterPage} />
+      <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
 
 export default App;
