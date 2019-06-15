39e414926ba283b99e1199a68dde5901ad510661
Clay Coleman <jclaytoncoleman@gmail.com>
2019-06-15 18:48:00 +0100

    password change

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+import { withFirebase } from '../Firebase';
+import IconModal, { ICON_STATES } from '../IconModal';
+
+const INITIAL_STATE = {
+  password: '',
+  confirmPassword: '',
+  error: null,
+
+  showModal: false,
+  modalText: '',
+  modalIcon: '',
+};
+
+class PasswordChangeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { password } = this.state;
+    this.setState({
+      showModal: true,
+      modalText: 'Loading...',
+      modalIcon: ICON_STATES.LOADING,
+    });
+
+    this.props.firebase
+      .doPasswordUpdate(password)
+      .then(() => {
+        this.setState({
+          ...INITIAL_STATE,
+          modalText: 'Reset password sent!',
+          modalIcon: ICON_STATES.SUCCESS,
+        });
+      })
+      .catch(error => {
+        this.setState({ error, modalText: 'Uh oh...', modalIcon: ICON_STATES.ERROR });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleModalFinished = () => {
+    // we should just hide the modal after it's done
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { password, confirmPassword, error, showModal, modalIcon, modalText } = this.state;
+
+    const isInvalid = password !== confirmPassword || password === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <IconModal
+          show={showModal}
+          text={modalText}
+          icon={modalIcon}
+          onExit={this.handleModalFinished}
+        />
+        ;
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="New Password"
+        />
+        <input
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm New Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Reset My Password
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+export default withFirebase(PasswordChangeForm);
