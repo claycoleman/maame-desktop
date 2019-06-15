ddde1d7b048239ca1ad78a0e5483e1495ca19be6
Clay Coleman <jclaytoncoleman@gmail.com>
2019-06-15 18:43:58 +0100

    (hopefully) added loading modal

--- src/components/IconModal/index.js
+++ src/components/IconModal/index.js
@@ -6,12 +6,25 @@ import { MdClear, MdInfoOutline, MdCheck } from 'react-icons/lib/md';
 export default class IconModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      previouslyLoading: false,
+      exitTimeoutStarted: false,
+    };
   }
 
   componentDidMount() {}
 
+  startExitTimeout = () => {
+    this.setState({ exitTimeoutStarted: true }, () => {
+      setTimeout(() => {
+        // if hideModal is provided, just hide the modal; else,
+        this.props.onExit(this.props.onExitParams);
+      }, 1250);
+    });
+  };
+
   render() {
+    const { previouslyLoading, exitTimeoutStarted } = this.state;
     let icon;
     switch (this.props.icon) {
       case 'loading':
@@ -29,6 +42,10 @@ export default class IconModal extends React.Component {
         icon = <MdCheck className="icon" color="white" size={90} />;
         break;
     }
+    if (this.props.icon !== 'loading' && previouslyLoading && !exitTimeoutStarted) {
+      // icon switched away from loading, and we haven't started the timeout yet
+      this.startExitTimeout();
+    }
     return (
       <CSSTransition
         in={this.props.show}
@@ -43,10 +60,12 @@ export default class IconModal extends React.Component {
           exitActive: styles.modalExitActive,
         }}
         onEntered={() => {
-          setTimeout(() => {
-            // if hideModal is provided, just hide the modal; else, 
-            this.props.onExit(this.props.onExitParams);
-          }, 1250);
+          if (this.props.icon === 'loading') {
+            // don't exit if loading
+            this.setState({ previouslyLoading: true });
+          } else {
+            this.startExitTimeout();
+          }
         }}
         unmountOnExit
       >
